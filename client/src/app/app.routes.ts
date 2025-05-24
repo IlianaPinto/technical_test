f6a5da82193887a7e4719b6d5f2e71b471a66771
IlianaPinto <iliana.pinto04@gmail.com>
2025-05-24 12:17:12 -0600

    removed guards

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AuthGuard } from './guards/auth.guard';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
-import { LoginGuard } from './guards/login.guard';
 import { OrdersComponent } from './components/orders/orders.component';
 import { CustomersComponent } from './components/customers/customers.component';
 import { AdminGuard } from './guards/admin.guard';
