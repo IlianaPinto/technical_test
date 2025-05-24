0a8f9f29b716bbd8254509d47acbefd5ca17bde8
IlianaPinto <iliana.pinto04@gmail.com>
2025-05-24 00:57:33 -0600

    fixed dinamic table

--- client/src/app/components/orders/orders.component.ts
+++ client/src/app/components/orders/orders.component.ts
@@ -3,15 +3,40 @@ import { OrdersService } from '../../services/orders.service';
 import { Order } from '../../interfaces/orders.interface';
 import { DataTableComponent } from '../table/table.component';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-orders',
-  imports: [DataTableComponent],
+  imports: [MatIconModule, DataTableComponent],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css',
 })
 export class OrdersComponent implements OnInit {
   orders = new MatTableDataSource<Order>();
+
+  columns = [
+    { columnDef: 'id', header: 'id', cell: (element: any) => `${element.id}` },
+    {
+      columnDef: 'customerName',
+      header: 'Nombre',
+      cell: (element: any) => `${element.customerName}`,
+    },
+    {
+      columnDef: 'total',
+      header: 'Monto',
+      cell: (element: any) => `${element.total}`,
+    },
+    {
+      columnDef: 'status',
+      header: 'Status',
+      cell: (element: any) => `${element.status}`,
+    },
+    {
+      columnDef: 'actions',
+      header: 'Acciones',
+      cell: () => '',
+    },
+  ];
 
   constructor(private ordersService: OrdersService) {}
 
--- client/src/app/components/table/table.component.ts
+++ client/src/app/components/table/table.component.ts
@@ -1,42 +1,41 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [MatTableModule, MatFormFieldModule, MatInputModule, CommonModule],
+  imports: [
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule,
+    MatButtonModule,
+  ],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
 export class DataTableComponent<T> {
-  @Input() dataSource!: MatTableDataSource<T>;
-  columns = [
-    { columnDef: 'id', header: 'id', cell: (element: any) => `${element.id}` },
-    {
-      columnDef: 'customerName',
-      header: 'Nombre',
-      cell: (element: any) => `${element.customerName}`,
-    },
-    {
-      columnDef: 'total',
-      header: 'Monto',
-      cell: (element: any) => `${element.total}`,
-    },
-    {
-      columnDef: 'status',
-      header: 'Status',
-      cell: (element: any) => `${element.status}`,
-    },
-    {
-      columnDef: 'actions',
-      header: 'Acciones',
-      cell: () => '',
-    },
-  ];
-  displayedColumns: string[] = this.columns.map((c) => c.columnDef);
+  @Input() columns: any[] = [];
+  @Input() dataSource!: MatTableDataSource<any>;
+  @Output() editar = new EventEmitter<any>();
+
+  get displayedColumns(): string[] {
+    return this.columns.map((c) => c.columnDef);
+  }
+
+  onEditar(element: any) {
+    this.editar.emit(element);
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
